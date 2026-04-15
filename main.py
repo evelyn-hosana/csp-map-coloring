645d0a3cfb7c15a78e4a97068fe88c971c074afc
Evelyn Hosana <evelynhosana123@gmail.com>
2026-04-15 15:16:45 -0400

    Initial Commit
    Basic setup of project 3... including:
    1) .gitignore to avoid uploading unwanted files to repo
    2) README.md as project description placeholder
    3) requirements.txt for dependency tracking
    4) main.py as minimal entry point for program to be updated

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+main.py - entry point for CSP map coloring.
+"""
+# imports here
+
+def main():
+    print("Hello, this will be used for CSP map coloring!")
+
+if __name__ == '__main__':
+    main()
